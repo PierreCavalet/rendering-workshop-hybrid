1d6f247429635445b430ade9cb65f4d69aa10db7
Pierre Cavalet <pcavalet@kaliop.com>
2023-06-15 10:05:30 +0200

    go

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const routes = ["/"].concat(
+  Array.from({ length: 10 }, (_, i) => i + 1).map((id) => `/pokemon/${id}`)
+);
+
+const routeRules = routes.reduce(
+  (rules: Record<string, Record<string, boolean>>, route) => {
+    rules[route] = { prerender: true };
+
+    return rules;
+  },
+  {}
+);
+
 export default defineNuxtConfig({
-  ssr: false,
+  routeRules,
 });
